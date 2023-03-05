58b3c37db79ac5a3252fea37d715ffee22e496d2
Pavel Gutsu <pavelgutsu@coherentsolutions.com>
2023-03-05 20:10:39 +0100

    Initial commit

---
+++ src/main/java/org/zodiac/coffee/ZodiacCoffeeApplication.java
@@ -0,0 +1,13 @@
+package org.zodiac.coffee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZodiacCoffeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZodiacCoffeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/zodiac/coffee/ZodiacCoffeeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zodiac.coffee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZodiacCoffeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
